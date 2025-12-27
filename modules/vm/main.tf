3872cb3cd7da61a949f48b2792958f99716b2cc7
Anil Katari <anilkataricloudengineer@gmail.com>
2025-12-27 20:01:00 +0530

    Roboshop Project

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -80,8 +80,8 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile {
     computer_name  = var.component
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
   }
   os_profile_linux_config {
     disable_password_authentication = false
@@ -91,6 +91,7 @@ resource "azurerm_virtual_machine" "main" {
   }
 
 }
+
 
 resource "null_resource" "ansible"{
 
--- provider.tf
+++ provider.tf
@@ -6,3 +6,9 @@ provider "azurerm" {
   features {}
   subscription_id = "52cd5f2c-0d28-4429-b80d-950089f20b98"
 }
+
+provider "vault" {
+  address = "http://vault-internal.anilcloudengineer.online:8200"
+  token = var.token
+}
+
