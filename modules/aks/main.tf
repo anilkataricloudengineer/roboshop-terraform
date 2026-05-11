c19009a844b58aeffd63fa4e40a952ecd0290604
anilkataricloudengineer <anilkataricloudengineer@gmail.com>
2026-05-11 16:13:56 +0530

    Roboshop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2_v2"
+    auto_scaling_enabled = true
   }
 
   identity {
