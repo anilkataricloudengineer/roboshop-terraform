3da22d03fe683437e70297ed5b85d63923207961
anilkataricloudengineer <anilkataricloudengineer@gmail.com>
2026-05-13 15:55:52 +0530

    Roboshop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -20,6 +20,11 @@ resource "azurerm_kubernetes_cluster" "main" {
     subnet_name = "/subscriptions/52cd5f2c-0d28-4429-b80d-950089f20b98/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
   }
 
+  network_profile {
+    network_plugin = "azure"
+    service_cidr = "10.100.0.0/24"
+  }
+
   identity {
     type = "SystemAssigned"
   }
