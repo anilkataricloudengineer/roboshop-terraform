8087ce2aa8d23e1b20ede1dc79496d494b9c9e21
anilkataricloudengineer <anilkataricloudengineer@gmail.com>
2026-05-13 15:34:30 +0530

    Roboshop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     auto_scaling_enabled = true
     min_count = 1
     max_count = 10
-    pod_subnet_id = "/subscriptions/52cd5f2c-0d28-4429-b80d-950089f20b98/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
+    # pod_subnet_id = "/subscriptions/52cd5f2c-0d28-4429-b80d-950089f20b98/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
     vnet_subnet_id = "/subscriptions/52cd5f2c-0d28-4429-b80d-950089f20b98/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
   }
 
