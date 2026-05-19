ce1167783e2313b9a42fe41a858f56c67876321f
anilkataricloudengineer <anilkataricloudengineer@gmail.com>
2026-05-19 23:36:45 +0530

    Roboshop Project

---
+++ modules/aks/helm.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = <<EOF
+az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name main --overwrite-existing
+kubectl apply -f vault-token.yml
+EOF
+  }
+}
+
+resource "helm_release" "external-secrets" {
+  name       = "external-secrets"
+  repository = "https://charts.external-secret.io"
+  chart      = "external-secrets"
+}
